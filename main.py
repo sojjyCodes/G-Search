a77c73c40876abd3c531cbf1324f334f8de971f0
Ebenezer <66065626+sojjyCodes@users.noreply.github.com>
2020-06-15 14:45:42 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#sojjyCodes
+import http.server
+import socketserver
+
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Listening on port", PORT)
+    httpd.serve_forever()
+
+
